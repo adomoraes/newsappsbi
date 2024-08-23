d3122b507822bb7e4f932c04425b0bb31d547cf7
Eduardo Moraes <adomoraes@gmail.com>
2024-08-23 09:01:20 -0300

    build: PWA configs

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
+
+export default defineConfig({
+	base: "/newsappsbi/",
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: [
+				"favicon.svg",
+				"favicon.ico",
+				"robots.txt",
+				"apple-touch-icon.png",
+			],
+			manifest: {
+				name: "NewsApp SBI",
+				short_name: "NewsApp",
+				description: "Newsletter SBI",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
+})

a3cbb9fd7a3464fd39bc77c35587a91f86c2b836
Eduardo Moraes <adomoraes@gmail.com>
2024-09-03 18:47:59 -0300

    Revert "feat: implementar tela de boas-vindas e configurar TailwindCSS"
    This reverts commit 7e69e520477fcc3685a1a0181c3144e50bbc267a.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {
-			colors: {
-				primary: "#081F87",
-				secondary: "#00BCE4",
-				accent: "#0C033D",
-				white: "#ffffff",
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 }

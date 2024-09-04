32d06b2e5948f875483e719ce296db096b728228
Eduardo Moraes <adomoraes@gmail.com>
2024-09-04 17:58:17 -0300

    feat: melhorar o tratamento da instalação do PWA e texto dinâmico do botão
    - Modificado src/pages/Welcome.jsx para aprimorar o tratamento da instalação do PWA.
    - Adicionado texto dinâmico no botão que muda conforme o estado de instalação do PWA.
    - Implementado redirecionamento para abrir o PWA caso já esteja instalado.
    - Melhoradas as verificações para o modo standalone do PWA em diferentes plataformas.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react"
 import HomePage from "./HomePage"
-// import FetchData from './FetchData'; // Descomente quando o componente FetchData estiver implementado
 
 const Welcome = () => {
 	const [deferredPrompt, setDeferredPrompt] = useState(null)
 	const [isInstalled, setIsInstalled] = useState(false)
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		// Verificar se a PWA está instalada
-		window.addEventListener("appinstalled", () => {
+		const handleAppInstalled = () => {
 			setIsInstalled(true)
-		})
+		}
+
+		window.addEventListener("appinstalled", handleAppInstalled)
 
 		// Captura o evento antes da instalação
 		window.addEventListener("beforeinstallprompt", (e) => {
@@ -18,20 +20,47 @@ const Welcome = () => {
 			setDeferredPrompt(e)
 		})
 
+		// Verificar manualmente se o PWA já está em modo standalone
+		if (
+			window.matchMedia("(display-mode: standalone)").matches ||
+			window.navigator.standalone
+		) {
+			setIsInstalled(true)
+		}
+
+		setLoading(false) // Encerrar o carregamento quando o estado inicial for determinado
+
 		return () => {
-			window.removeEventListener("appinstalled", () => setIsInstalled(false))
+			window.removeEventListener("appinstalled", handleAppInstalled)
 			window.removeEventListener("beforeinstallprompt", () =>
 				setDeferredPrompt(null)
 			)
 		}
 	}, [])
 
-	const handleInstallClick = () => {
-		if (deferredPrompt) {
+	useEffect(() => {
+		if (isInstalled) {
+			// Mostrar loading por 3 segundos se a PWA estiver instalada
+			setTimeout(() => {
+				setLoading(false)
+			}, 3000)
+		} else {
+			setLoading(false)
+		}
+	}, [isInstalled])
+
+	const handleButtonClick = () => {
+		if (isInstalled) {
+			// Se o PWA já está instalado, tente abrir o aplicativo
+			window.location.href = "/"
+		} else if (deferredPrompt) {
+			// Se o PWA não está instalado, mostramos o prompt de instalação
 			deferredPrompt.prompt()
 			deferredPrompt.userChoice.then((choiceResult) => {
 				if (choiceResult.outcome === "accepted") {
-					console.log("Usuário aceitou a instalação da PWA")
+					console.log("Usuário aceitou o prompt de instalação.")
+				} else {
+					console.log("Usuário recusou o prompt de instalação.")
 				}
 				setDeferredPrompt(null)
 			})
@@ -43,45 +72,53 @@ const Welcome = () => {
 		return /iphone|ipad|ipod/.test(userAgent)
 	}
 
-	const isInStandaloneMode = () =>
-		"standalone" in window.navigator && window.navigator.standalone
+	const isInStandaloneMode = () => {
+		return (
+			window.matchMedia("(display-mode: standalone)").matches ||
+			window.navigator.standalone
+		)
+	}
 
-	return (
-		<>
-			{isInstalled && isInStandaloneMode && <HomePage />}
-			{!isInstalled && deferredPrompt && (
-				<div className='flex flex-col items-center justify-center h-screen bg-[#081F87] text-white text-center p-6'>
-					<img
-						className='rounded-full p-7 mb-7 bg-white mx-auto'
-						src='./assets/images/logo.png'
-						alt='Logo SBI'
-					/>
-					<h1 className='text-4xl font-bold'>Bem-vindo ao NewsAPP SBI</h1>
-					<h2 className='text-2xl mt-4'>
-						O essencial da infectologia na suas mãos
-					</h2>
-					<p className='mt-2'>
-						Fique atualizado com as últimas notícias, eventos e informações
-						importantes sobre infectologia diretamente da Sociedade Brasileira
-						de Infectologia.
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
+	if (isInstalled) {
+		return <HomePage />
+	} else {
+		return (
+			<div className='flex flex-col items-center justify-center h-screen bg-[#081F87] text-white text-center p-6'>
+				<img
+					className='rounded-full p-7 mb-7 bg-white mx-auto'
+					src='./assets/images/logo.png'
+					alt='Logo SBI'
+				/>
+				<h1 className='text-4xl font-bold'>Bem-vindo ao NewsAPP SBI</h1>
+				<h2 className='text-2xl mt-4'>
+					O essencial da infectologia na suas mãos
+				</h2>
+				<p className='mt-2'>
+					Fique atualizado com as últimas notícias, eventos e informações
+					importantes sobre infectologia diretamente da Sociedade Brasileira de
+					Infectologia.
+				</p>
+				{isIos() && !isInStandaloneMode() && (
+					<p className='mt-2 text-center'>
+						Para instalar o aplicativo, clique no ícone de compartilhar no
+						navegador e selecione "Adicionar à Tela de Início".
 					</p>
-					{isIos() &&
-						!isInStandaloneMode()(
-							<p className='mt-2 text-center'>
-								Para instalar o aplicativo, clique no ícone de compartilhar no
-								navegador e selecione "Adicionar à Tela de Início".
-							</p>
-						)}
+				)}
 
-					<button
-						className='mt-6 px-4 py-2 bg-[#00BCE4] text-[#0C033D] rounded hover:bg-[#0C033D] hover:text-[#00BCE4] transition duration-300'
-						onClick={handleInstallClick}>
-						Clique aqui para instalar!
-					</button>
-				</div>
-			)}
-		</>
-	)
+				<button
+					className='mt-6 px-4 py-2 bg-[#00BCE4] text-[#0C033D] rounded hover:bg-[#0C033D] hover:text-[#00BCE4] transition duration-300'
+					onClick={handleButtonClick}>
+					{isInstalled
+						? "Clique aqui para abrir o aplicativo!"
+						: "Clique aqui para instalar!"}
+				</button>
+			</div>
+		)
+	}
 }
 
 export default Welcome

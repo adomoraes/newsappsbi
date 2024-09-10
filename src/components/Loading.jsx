0a26258969d64f1a46adb693b07496067a65f9db
Eduardo Moraes <adomoraes@gmail.com>
2024-09-10 12:16:22 -0300

    feat: implementar lógica de carregamento e instalação do PWA
    - Adiciona componente <Loading> que exibe uma tela de carregamento enquanto o app está sendo instalado
    - Ajusta o temporizador para exibir o carregamento por 3 segundos após a instalação
    - Integra detecção do modo standalone e exibe a <HomePage> ao finalizar a instalação
    - Adiciona botão Acessar o APP com comportamento de clique após a instalação

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,10 +2,22 @@ import React from "react"
 
 const Loading = ({ successMessage }) => {
 	return (
-		<div className='flex items-center justify-center h-screen'>
+		<div className='flex flex-col items-center justify-center h-screen bg-[#081F87] text-white text-center p-6'>
+			<img
+				className='rounded-full p-7 mb-7 bg-white mx-auto'
+				src='./assets/images/logo.png'
+				alt='Logo SBI'
+			/>
 			<div className='loading-spinner'>
 				{successMessage ? (
-					<p>{successMessage}</p>
+					<>
+						<p>{successMessage}</p>
+						<button
+							onClick={handleReload}
+							className='mt-6 px-4 py-2 bg-[#00BCE4] text-[#0C033D] rounded hover:bg-[#0C033D] hover:text-[#00BCE4] transition duration-300'>
+							Abrir aplicativo
+						</button>
+					</>
 				) : (
 					<p>Instalando o aplicativo...</p>
 				)}
